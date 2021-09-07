c70fda5215757b544425384e96c1a2080f6c994a
Papryq <piq1998pll@wp.pl>
2021-09-07 17:07:01 +0200

    Array update

--- project1/src/views/Map.vue
+++ project1/src/views/Map.vue
@@ -48,14 +48,84 @@ export default {
             name: 'Brimstone',
             path: '@/assets/Brimstone1.png'
           },
+          {
+            id: '2',
+            name: 'Omen',
+            path: '@/assets/Omen1.png'
+          },
+          {
+            id: '3',
+            name: 'Killjoy',
+            path: '@/assets/Killjoy1.png'
+          },
+                    {
+            id: '4',
+            name: 'Cypher',
+            path: '@/assets/Cypher1.png'
+          },
+                    {
+            id: '5',
+            name: 'Sova',
+            path: '@/assets/Sova1.png'
+          },
+                    {
+            id: '6',
+            name: 'Sage',
+            path: '@/assets/Sage1.png'
+          },
+                    {
+            id: '7',
+            name: 'Pheoenix',
+            path: '@/assets/Pheoenix1.png'
+          },
+                    {
+            id: '8',
+            name: 'Jett',
+            path: '@/assets/Jett1.png'
+          },
+                    {
+            id: '9',
+            name: 'Reyna',
+            path: '@/assets/Reyna1.png'
+          },
+                    {
+            id: '10',
+            name: 'Raze',
+            path: '@/assets/Raze1.png'
+          },
+                    {
+            id: '11',
+            name: 'Breach',
+            path: '@/assets/Breach1.png'
+          },
+          {
+            id: '12',
+            name: 'Skye',
+            path: '@/assets/Skye1.png'
+          },
+          {
+            id: '13',
+            name: 'Yoru',
+            path: '@/assets/Yoru1.png'
+          },
+          {
+            id: '14',
+            name: 'Astra',
+            path: '@/assets/Astra1.png'
+          },
+          {
+            id: '15',
+            name: 'Kayo',
+            path: '@/assets/Kayo1.png'
+          },
         ]
       },
 
       setMaps() {
         this.maps = [
           {
-            id: '1',
-            name: 'Brimstone',
+            id: 'Ascent',
+            name: 'Ascent',
             path: '@/assets/mapAscent3.png',
             bestAgents: [
               {
@@ -73,6 +143,41 @@ export default {
                 id:'4'
               }
             ]
+          },
+          {
+            id: 'Haven',
+            name: 'Haven',
+            path: '',
+            bestAgents: [],
+            worstAgents: [],
+          },
+          {
+            id: 'Split',
+            name: 'Split',
+            path: '',
+            bestAgents: [],
+            worstAgents: [],
+          },
+          {
+            id: 'Bind',
+            name: 'Bind',
+            path: '',
+            bestAgents: [],
+            worstAgents: [],
+          },
+          {
+            id: 'Icebox',
+            name: 'Icebox',
+            path: '',
+            bestAgents: [],
+            worstAgents: [],
+          },
+          {
+            id: 'Breeze',
+            name: 'Breeze',
+            path: '',
+            bestAgents: [],
+            worstAgents: [],
           },
         ]
       }
