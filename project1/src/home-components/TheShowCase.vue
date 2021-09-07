27108e978714d4818f86818fbade418738d480ac
Artur Pabjan <artur.pabjan00@gmail.com>
2021-09-07 14:23:17 +0200

    fixed images and icons

--- project1/src/home-components/TheShowCase.vue
+++ project1/src/home-components/TheShowCase.vue
@@ -1,8 +1,8 @@
 <template>
-        <section class=" text-light p-5 my-2">
+        <section class="text-light p-5 my-2">
             <div class="container">
                <div class="d-sm-flex justify-content-between">
-                  <img class="img-fluid w-50 d-none d-sm-block omenAnimation" src="/photos/photo1.png" alt="">
+                  <img class="img-fluid w-50 d-none d-sm-block omenAnimation" src="@/assets/photo1.png" alt="">
                       <div class="text-center my-3">
                          <h1>Pamiętaj, że <span id="show" class="text-warning">przystojniakiem, nie zmieniaj się</span></h1>
                          <p class="lead my-3 p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem explicabo voluptatibus voluptatum officia quaerat architecto esse nesciunt tempore deserunt animi, perferendis, similique qui? Nobis ex quod in doloribus voluptatibus voluptates! </p>
