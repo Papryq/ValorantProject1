48d25b1c394d015d453f5003d523153f47131859
Papryq <piq1998pll@wp.pl>
2021-09-14 17:37:45 +0200

    mouseover
    1

--- project1/src/components/home-components/TheBoxes.vue
+++ project1/src/components/home-components/TheBoxes.vue
@@ -1,39 +1,31 @@
 <template>
           <section>
-            <div class="container">
-              <div class="row text-center py-3">
-                <div class="col-md">
-                  <div   @mouserover="actionHover()">
-                    <video width="320" height="240">
+              <div class="row text-center">
+                <div class="col-md-3">
+                    <video width="200" height="280" loop="" @mouseover="videoControl" @mouseleave="videoControlStop">
                       <source src="@/assets/Video/fela.mp4" type="video/mp4">
                       <source src ="@/assets/Video/fela.mp4" type="video/ogg">
                     </video>              
-                  </div>
                 </div>
-                <div class="col-md">
-                           <div class="player-container">
-                            <vue3-video-player @play="your_method" src="./videos/your_video.mp4"></vue3-video-player>
-                            </div>       
+                <div class="col-md-3">
+                    <video width="200" height="280" loop="" @mouseover="videoControl" @mouseleave="videoControlStop">
+                      <source src="@/assets/Video/fela.mp4" type="video/mp4">
+                      <source src ="@/assets/Video/fela.mp4" type="video/ogg">
+                    </video>   
                   </div>
-                </div>
-                 <div class="col-md">
-                  <div class="card shadow-lg" style="width: 18rem;">
-                    <img src="" class="card-img-top" alt="">
-                    <div class="card-body">
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                  </div>
+                 <div class="col-md-3">
+                    <video width="200" height="280" loop="" @mouseover="videoControl" @mouseleave="videoControlStop">
+                      <source src="@/assets/Video/fela.mp4" type="video/mp4">
+                      <source src ="@/assets/Video/fela.mp4" type="video/ogg">
+                    </video>   
                  </div>
-                 <div class="col-md">
-                  <div class="card shadow-lg" style="width: 18rem;">
-                    <img src="" class="card-img-top" alt="">
-                    <div class="card-body">
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                  </div>
+                 <div class="col-md-3">
+                    <video width="200" height="280" loop="" @mouseover="videoControl" @mouseleave="videoControlStop">
+                      <source src="@/assets/Video/fela.mp4" type="video/mp4">
+                      <source src ="@/assets/Video/fela.mp4" type="video/ogg">
+                    </video>   
                  </div>
               </div>
-            </div>
           </section>
 </template>
 
@@ -41,15 +33,34 @@
 
 export default {
     name: 'TheBoxes',
+      methods: {
+        videoControl() {
+          const videoPlayer = document.querySelector('video');
+            if(videoPlayer.paused){
+              videoPlayer.play();
+            }
+            else {
+              videoControlStop = videoPlayer.currentTime = 0;
+            }
+        }
+      }
+
 }
 
 </script>
 
 <style scoped>
  .col-md {
-   padding: 0 10px 40px 10px;
+   padding: 0 5px 40px 10px;
  }
+video {
+  cursor: pointer;
+}
 
+.col-md-3 {
+  border: 1px solid #212529;
+  border-radius: 10%;
+}
 
 
 </style>
